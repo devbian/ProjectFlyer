b380eda6ae9fe5f99056cc28a3281304ba42e88c
phnix <iamphnix@foxmail.com>
2016-06-12 16:06:50 +0800

    fix route position bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,6 +4,6 @@ Route::get('/', function () {
     return view('pages.home');
 });
 
+Route::resource('/flyers', 'FlyersController');
 Route::post('/{zip}/{street}/photos', ['as' => 'store_photo_path', 'uses' => 'FlyersController@addPhoto']);
 Route::get('/{zip}/{street}', 'FlyersController@show');
-Route::resource('/flyers', 'FlyersController');
